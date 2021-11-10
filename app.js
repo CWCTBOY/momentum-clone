c3833d936e0f8b1e4d7efb0ab16ca98bd2b5bc12
CWCTBOY <qkrdlswo98@gmail.com>
2021-11-10 23:49:24 +0900

    event-preventDefault

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const={
+const loginForm = document.querySelector(".login");
+const loginInput = document.querySelector(".login .login_input");
 
+function submitLoginForm(event) {
+  event.preventDefault();
+  console.log("Hello", loginInput.value + ".")
 }
+
+loginForm.addEventListener("submit", submitLoginForm);
