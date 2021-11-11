3e46759b7ca9e78f3179cc2b8e72dc615b784099
CWCTBOY <qkrdlswo98@gmail.com>
2021-11-11 17:24:31 +0900

    localStorage conditional

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const loginInput = document.querySelector(".login_input");
 const sayHello = document.querySelector(".sayhello");
 
 const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
 
-
-function loginInputSubmit(event) {
+function loginSubmit(event) {
   const userName = loginInput.value;
   event.preventDefault();
+  localStorage.setItem(USERNAME_KEY, userName);
   console.log(`Hello, ${userName}!`);
   logninForm.classList.add(HIDDEN_CLASS);
   sayHello.classList.remove(HIDDEN_CLASS);
   sayHello.innerHTML = `Hello, ${userName}!`
-};
+}
 
-logninForm.addEventListener("submit", loginInputSubmit);
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if (savedUserName === null)
+{
+  logninForm.classList.remove(HIDDEN_CLASS);
+  logninForm.addEventListener("submit", loginSubmit);
+} else
+{
+  sayHello.innerHTML = `Hello, ${savedUserName}!`;
+  sayHello.classList.remove(HIDDEN_CLASS);
+}
