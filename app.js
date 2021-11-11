acd3be159bbc599d95c0d6fb10b07dacf01226c4
CWCTBOY <qkrdlswo98@gmail.com>
2021-11-11 15:46:39 +0900

    a simple login

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const loginForm = document.querySelector(".login");
-const loginInput = document.querySelector(".login .login_input");
+const logninForm = document.querySelector(".login");
+const loginInput = document.querySelector(".login_input");
+const display = document.querySelector(".display");
 
-function submitLoginForm(event) {
+const HIDDEN_CLASS = "hidden";
+
+
+function loginInputSubmit(event) {
+  const userName = loginInput.value;
   event.preventDefault();
-  console.log("Hello", loginInput.value + ".")
-}
+  console.log("Hello,", userName + ".");
+  logninForm.classList.add(HIDDEN_CLASS);
+  display.classList.remove(HIDDEN_CLASS);
+  display.innerHTML = `Hello, ${userName}. `
+};
 
-loginForm.addEventListener("submit", submitLoginForm);
+logninForm.addEventListener("submit", loginInputSubmit);
