c4d4a7342f4b53a4267d78b93fc41976d3c3b023
CWCTBOY <qkrdlswo98@gmail.com>
2021-11-11 15:51:49 +0900

    update backticks

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const HIDDEN_CLASS = "hidden";
 function loginInputSubmit(event) {
   const userName = loginInput.value;
   event.preventDefault();
-  console.log("Hello,", userName + ".");
+  console.log(`Hello, ${userName}!`);
   logninForm.classList.add(HIDDEN_CLASS);
   display.classList.remove(HIDDEN_CLASS);
-  display.innerHTML = `Hello, ${userName}. `
+  display.innerHTML = `Hello, ${userName}!`
 };
 
 logninForm.addEventListener("submit", loginInputSubmit);
