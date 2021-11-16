04694d17bd62ef1842f8a2e25093672c4f58efcf
CWCTBOY <qkrdlswo98@gmail.com>
2021-11-16 15:06:31 +0900

    not a masterpiece but a Thing that is need to be modify

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,33 @@ const toDoForm = document.querySelector(".todo");
 const toDoList = document.querySelector(".todo-list");
 const toDoText = document.querySelector(".todo_text");
 
-function submitToDoForm(event) {
-  event.preventDefault();
-  const toDO = toDoText.value;
-  localStorage.setItem("todo", toDO);
-  toDO = ``;
+function savedToDo() {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const btn = document.createElement("button");
+  toDoList.appendChild(li);
+  li.appendChild(span);
+  li.appendChild(btn);
+  span.innerHTML = `${typedToDo}`;
 };
 
-toDoForm.addEventListener("submit", submitToDoForm);
+const typedToDo = localStorage.getItem("todo");
+
+if (typedToDo === null)
+{
+  toDoForm.addEventListener("submit", submitToDoForm);
+} else
+{
+  savedToDo();
+}
+
+function submitToDoForm(event) {
+  event.preventDefault();
+  const toDo = toDoText.value;
+  localStorage.setItem("todo", toDo);
+  toDoText.value = ``;
+  savedToDo(typedToDo);
+};
+
+
+
