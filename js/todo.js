59d146def95ab5fa4a8d0d7077a71b26ad03a78a
CWCTBOY <qkrdlswo98@gmail.com>
2021-11-17 22:14:02 +0900

    localstorage 'not complete'

--- js/todo.js
+++ js/todo.js
@@ -2,21 +2,29 @@ const toDoForm = document.querySelector(".todo");
 const toDoInput = document.querySelector(".todo_text");
 const toDoList = document.querySelector(".todo-list");
 
+const savedToDo = [];
+
 toDoForm.addEventListener("submit", submitToDoForm);
 
 function submitToDoForm(event) {
   event.preventDefault();
   const typedToDo = toDoInput.value;
   toDoInput.value = ``;
+  savedToDo.push(typedToDo);
   paintToDo(typedToDo);
+  pushLocalStorage();
 };
 
-function paintToDo(a) {
+function pushLocalStorage() {
+  localStorage.setItem("todo", JSON.stringify(savedToDo));
+};
+
+function paintToDo(typedToDo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   toDoList.appendChild(li);
   li.appendChild(span);
-  span.innerHTML = a;
+  span.innerHTML = typedToDo;
   const btn = document.createElement("button");
   li.appendChild(btn);
   btn.innerHTML = `↩️`;
@@ -26,4 +34,4 @@ function paintToDo(a) {
 function removeToDo(event) {
   const removeToDo = event.target.parentElement;
   removeToDo.remove();
-}
+};
