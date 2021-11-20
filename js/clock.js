2fea485f088ad925b672ddfbd35915ee18b359e2
CWCTBOY <qkrdlswo98@gmail.com>
2021-11-21 05:29:12 +0900

    update

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,8 @@ function liveClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  clock.innerHTML = `${hours}:${minutes}`;
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerHTML = `${hours}:${minutes}:${seconds}`;
 };
 
 liveClock();
