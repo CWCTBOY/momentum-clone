bce9e714da494301334bea6378c07c008b83de20
CWCTBOY <qkrdlswo98@gmail.com>
2021-11-26 15:54:48 +0900

    background

--- js/background.js
+++ js/background.js
@@ -1,5 +1,9 @@
 const bgi = [
-  "https://images2.alphacoders.com/480/thumb-1920-480530.png"
+  "https://p4.wallpaperbetter.com/wallpaper/166/117/196/anarchy-anonymous-dark-hacker-wallpaper-preview.jpg",
+  "https://www.teahub.io/photos/full/88-885387_android-developer-wallpaper-hd.jpg",
+  "https://birchtree.nyc3.digitaloceanspaces.com/images/wwdc18/dev-dots.png",
+  "https://images2.alphacoders.com/480/thumb-1920-480530.png",
+  "https://images8.alphacoders.com/115/thumb-1920-1156488.png"
 ];
 
 function intervalBgi() {
